36c3d797b772111d2e6ce94b0cf156d81cb93e65
Luke Beretta <Luke@Lukes-Air.Home>
2018-02-13 16:16:02 +0200

    First wesbite upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+// Smooth Scrolling
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 800);
+});
